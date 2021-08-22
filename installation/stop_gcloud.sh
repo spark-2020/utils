53d8452b06e898f7a603d755efe02fd098dda1a2
spark-2020 <89333637+spark-2020@users.noreply.github.com>
2021-08-22 21:02:04 +0800

    Update stop_gcloud.sh

--- installation/stop_gcloud.sh
+++ installation/stop_gcloud.sh
@@ -4,4 +4,3 @@ gcloud compute instances update tutorial-dev-vm --no-deletion-protection -q --zo
 gcloud compute instances delete tutorial-dev-vm -q --zone asia-southeast1-a
 gcloud compute addresses delete tutorial-dev-vm-ip --region=asia-southeast1 -q
 gcloud compute firewall-rules delete allow-http -q
-gcloud compute firewall-rules delete allow-https -q
