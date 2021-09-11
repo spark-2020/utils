444873c29c6525001a3de7022626cd462b9a9f35
spark-2020 <89333637+spark-2020@users.noreply.github.com>
2021-09-11 14:10:08 +0800

    Add files via upload

---
+++ create_dataproc_cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+git clone https://github.com/spark-2020/utils.git
+
+git clone https://github.com/GoogleCloudDataproc/initialization-actions.git
+
+REGION='us-central1'
+CLUSTER_NAME='test-wordcount'
+gcloud dataproc clusters create ${CLUSTER_NAME} \
+    --region ${REGION} \
+    --initialization-actions gs://goog-dataproc-initialization-actions-${REGION}/connectors/connectors.sh \
+    --metadata GCS_CONNECTOR_VERSION=2.2.2 \
+    --metadata bigquery-connector-version=1.2.0 \
+    --metadata spark-bigquery-connector-version=0.21.0
